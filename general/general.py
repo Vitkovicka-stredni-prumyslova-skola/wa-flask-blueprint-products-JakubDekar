a40927e22f1bf2c2b22f269909f0c7dfd799425c
Jakub Dekař <jakub.dekar@student.vitkovickastredni.cz>
2024-02-29 13:15:52 +0100

    CV-selected category

--- general/general.py
+++ general/general.py
@@ -7,3 +7,8 @@ general_bp = Blueprint('general_bp', __name__,
 @general_bp.route('/')
 def index():
     return render_template('general/index.html')
+
+categories = ['Mužské ', 'Ženské', 'zlato'] 
+@general_bp.route('/categories')
+def display_categories():
+    return render_template('index.html', categories=categories)
--- products/products.py
+++ products/products.py
@@ -4,6 +4,7 @@ products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
+
 @products_bp.route('/products')
 def index():
     data = GetAllProducts()
